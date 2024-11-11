20a03e20905e65cb3649b491870e94abccb30bbd
asmaa <asmaa@DESKTOP-Q29Q9S5>
2024-11-11 10:06:50 +0100

    local n tournament

---
+++ game/TournamentLocal.js
@@ -0,0 +1,34 @@
+// TournamentLocal.jsx
+import React from "react";
+import "./TournamentLocal.css";
+
+function TournamentLocal() {
+  return (
+    <div className="TournamentLocal-background">
+        <div className="TournamentLocal-container">
+            <div className="TournamentLocal-container">
+                <div className="TournamentLocal-icon"></div>
+                name<input className="name"/>
+            </div>
+            <div className="TournamentLocal-container">
+                <div className="TournamentLocal-icon"></div>
+                <input className="name"name/>
+            </div>
+        </div>
+        <span class="v">V</span>
+        <span class="s">S</span>
+        <div className="TournamentLocal-container">
+            <div className="TournamentLocal-container">
+                <div className="TournamentLocal-icon"></div>
+                <input className="name"name/>
+            </div>
+            <div className="TournamentLocal-container">
+                <div className="TournamentLocal-icon"></div>
+                <input className="name"name/>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default TournamentLocal;
